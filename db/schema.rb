809db41f29716641ad33cf2adffb660675d398b0
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-07 10:12:40 +0900

    Removing Test scaffold and Task created instead

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207010734) do
+ActiveRecord::Schema.define(version: 20170207011149) do
 
-  create_table "tests", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
